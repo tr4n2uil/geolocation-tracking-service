41d3cc75d5faedad594060d10f08f83ba67c6e0a
Vibhaj Rajan <vibhaj8@gmail.com>
2016-09-10 18:04:25 +0530

    added extra params check

--- app.js
+++ app.js
@@ -31,11 +31,27 @@ var validateFloat = function(value, response, name){
   return parsedValue;
 }
 
+var validateQuery = function(source, expected, response){
+  for(var i in expected){
+    if(source.indexOf(expected[i]) == -1){
+      server.sendResponse(response, "Invalid "+expected[i]);
+      return false;
+    }
+  }
+  if(source.length != expected.length){
+    server.sendResponse(response, "Extra parameters passed");
+    return false;
+  }
+  return true;
+}
+
 // ===== HANDLERS =====
 
 var initHandlers = function(db){
   // track
   server.handlers['track'] = function(request, response, url){
+    if(!validateQuery(Object.keys(url.query), ["device_id", "timestamp", "latitude", "longitude"], response)) return;
+
     var timestamp = validateInteger(url.query.timestamp, response, "timestamp");
     if(timestamp === false) return;
 
@@ -54,14 +70,16 @@ var initHandlers = function(db){
 
   // history
   server.handlers['history'] = function(request, response, url){
+    url.query.page = url.query.page || 0;
+    if(!validateQuery(Object.keys(url.query), ["device_id", "timestamp_start", "timestamp_end", "type", "page"], response)) return;
+
     var timestamp_start = validateInteger(url.query.timestamp_start, response, "timestamp_start");
     if(timestamp_start === false) return;
 
     var timestamp_end = validateInteger(url.query.timestamp_end, response, "timestamp_end");
     if(timestamp_end === false) return;
 
-    var page = url.query.page || 0;
-    page = validateInteger(page, response, "page");
+    var page = validateInteger(url.query.page, response, "page");
     if(page === false) return;
 
     if(["latlong", "geolocation"].indexOf(url.query.type) == -1)
